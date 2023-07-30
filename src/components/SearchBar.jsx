abfd4cb1b97840a32b378c42a8ddd173f6acf60f
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2023-07-30 11:24:58 +0530

    made videos appear

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,24 +4,26 @@ import { Paper, IconButton } from "@mui/material";
 import { Search } from "@mui/icons-material";
 const SearchBar = () => {
   return (
-    <Paper 
-    component="form" 
-    onSubmit={() => {}} 
-    sx={{ 
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
         borderRadius: 20,
-        border:'1px solid #e3e3e3', 
+        border: "1px solid #e3e3e3",
         pl: 2,
-        boxShadow:"none",
-        mr:{ sm : 5}
-    }}>
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
       <input
         className="search-bar"
         placeholder="Search"
         value=""
-        onChange={()=>{}}
+        type="text"
+        onChange={() => {}}
       />
-      <IconButton type="submit" sx={{p:'5px', color:'red'}}> 
-        <Search/>
+      <IconButton type="submit" sx={{ p: "5px", color: "red" }}>
+        <Search />
       </IconButton>
     </Paper>
   );
